e70285ae884325d458a86b6270120ebf31684571
iwindd <freewinendlife@gmail.com>
2023-08-26 19:25:50 +0700

    check permission

--- src/components/shake.tsx
+++ src/components/shake.tsx
@@ -1,6 +1,14 @@
 import { Vibration } from 'react-native';
+import { usePermissions } from '../contexts/permission';
+
 
 export const TodoStatusChangedShake = () => {
+    const { hasVibrationPermission} = usePermissions();
+
+    if (!hasVibrationPermission) {
+        return false
+    }
+
     const pattern = [200, 100, 200];
 
     Vibration.vibrate(pattern);
